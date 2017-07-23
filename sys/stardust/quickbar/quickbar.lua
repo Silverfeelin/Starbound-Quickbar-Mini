0f896a11951e2e023dd3b3d2cc15c50062af8e0f
Silverfeelin <Silverfeelin@gmail.com>
2017-07-23 18:45:07 +0200

    Revised to match StardustLib Quickbar
    With commit
    https://github.com/zetaPRIME/sb.StardustSuite/tree/ec45447620c1dd8866276b74eca5c7cf31e50ecf
    and changes in Starbound 1.3.2, the differences between StardustLib's
    Quickbar and Quickbar Mini are no longer necessary. The files are now
    identical (and should be able to stay identical).

--- sys/stardust/quickbar/quickbar.lua
+++ sys/stardust/quickbar/quickbar.lua
@@ -1,80 +1,82 @@
 --
 
+
+
+
 function openInterface(info)
-  -- Type checking
-  if type(info) == "string" then
-    info = { config = root.assetJson(info) }
-  elseif type(info) ~= "table" then
-    sb.logError("Quickbar: Interface '%s' could not be opened. Expected a string or table.", info)
-    restoreItem()
-    return
-  end
-
-  -- Globally store configuration.
-  if type(info.config) == "string" then
-    quickbarConfig = root.assetJson(info.config)
-  elseif type(info.config) == "table" then
-    quickbarConfig = info.config
-  end
-
-  -- Allow dynamic modification of the loaded configuration through global 'quickbarConfig'.
-  if quickbarConfig and info.loadScript then
-    loadScript(info.loadScript)
-  end
-
-  -- Open interface.
-  if quickbarConfig then
-    player.interact(info.interactionType or "ScriptPane", quickbarConfig)
-  else
-    sb.logError("Quickbar: Couldn't open an interface, as no valid config was defined.\nInfo: %s", sb.printJson(info))
-  end
+  if type(info) ~= "table" then info = {config = info} end
+  player.interact(info.interactionType or "ScriptPane", info.config)
 end
 
-local wList = "scroll.list"
 
-function addItem(item, prefix)
-  prefix = type(prefix) == "string" and prefix or ""
-  local label = string.format("%s%s", prefix, item.label)
 
-  local wLi = string.format("%s.%s", wList, widget.addListItem(wList))
-  local wLabel = string.format("%s.%s", wLi, "label")
-  local wBtnContainer = string.format("%s.%s", wLi, "buttonContainer")
 
-  widget.setText(wLabel, label)
-  widget.registerMemberCallback(wBtnContainer, "click", function()
-    openInterface({ config = item.pane, loadScript = item.loadScript })
+modules = {}
+
+local function handleClick(itm)
+  if itm.scriptAction then -- scripted action specified
+    local ci = string.find(itm.scriptAction, ":")
+    local module = string.sub(itm.scriptAction, 1, ci-1)
+    local action = string.sub(itm.scriptAction, ci+1)
+
+    if not modules[module] then
+      modules[module] = {} -- initialize module table and load in the appropriate script
+      _ENV.module = modules[module] -- allow code to be less dependent on filename
+      require(string.format("/quickbar/%s.lua", module))
+      _ENV.module = nil
+    end
+
+    if modules[module][action] then
+      modules[module][action](itm) -- trigger script action, passing in the item table
+    end
+  elseif itm.pane then openInterface(itm.pane) end
+end
+
+local lst = "scroll.list"
+
+local prefix = ""
+
+local function addItem(itm)
+  local li = lst .. "." .. widget.addListItem(lst)
+  widget.setText(li .. ".label", prefix .. itm.label)
+  widget.registerMemberCallback(li .. ".buttonContainer", "click", function()
+    handleClick(itm)
   end)
-
-  local wBtn = string.format("%s.%s.%s", wBtnContainer, widget.addListItem(wBtnContainer), "button")
-  if item.icon then
-    local icon = item.icon
-    if icon:sub(1,1) ~= "/" then icon = "/quickbar/" .. icon end
-    widget.setButtonOverlayImage(wBtn, icon)
+  local btn = li .. ".buttonContainer." .. widget.addListItem(li .. ".buttonContainer") .. ".button"
+  if itm.icon then
+    local icn = itm.icon
+    if icn:sub(1,1) ~= "/" then icn = "/quickbar/" .. icn end
+    widget.setButtonOverlayImage(btn, itm.icon)
   end
 end
 
-function addItems(items, prefix)
-  if type(items) ~= "table" or #items == 0 then return end
-
-  for _,v in ipairs(items) do
-    addItem(v, prefix)
-  end
-end
-
+local items = {}
+local autoRefreshRate = 0
+local autoRefreshTimer = 0
 function init()
-  widget.clearListItems(wList)
+  items = root.assetJson("/quickbar/icons.json") or {}
+  refresh()
 
-  local items = root.assetJson("/quickbar/icons.json") or {}
-  addItems(items.priority, "^#7fff7f;")
-  if player.isAdmin() then
-    addItems(items.admin, "^#bf7fff;")
-  end
-  addItems(items.normal)
+  autoRefreshRate = config.getParameter("autoRefreshRate")
+  autoRefreshTimer = autoRefreshRate
 end
 
-function loadScript(script)
-  local status, err = pcall(function() require(script) end)
-  if not status then
-    sb.logError("Quickbar: Failed loading '%s':\n%s", script, err)
+function refresh()
+  widget.clearListItems(lst)
+  prefix = "^#7fff7f;"
+  for k,v in pairs(items.priority or {}) do addItem(v) end
+  if player.isAdmin() then
+    prefix = "^#bf7fff;"
+    for k,v in pairs(items.admin or {}) do addItem(v) end
+  end
+  prefix = ""
+  for k,v in pairs(items.normal or {}) do addItem(v) end
+end
+
+function update(dt)
+  autoRefreshTimer = math.max(0, autoRefreshTimer - dt)
+  if autoRefreshTimer == 0 then
+    autoRefreshTimer = autoRefreshRate
+    --refresh() -- whoops, that just kind of derps things up :(
   end
 end
