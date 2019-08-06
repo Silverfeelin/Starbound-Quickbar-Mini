476c6990910173c1edb1f9ad9124533e88add7bb
Alberto Rota <alberto.rota.jimenez@gmail.com>
2019-08-06 11:48:04 +0200

    Added "not" and "hasCompletedQuest" conditions

--- sys/stardust/quickbar/quickbar.lua
+++ sys/stardust/quickbar/quickbar.lua
@@ -43,12 +43,16 @@ function conditions.all(...)
   for _, c in pairs{...} do if not condition(table.unpack(c)) then return false end end
   return true
 end
+function conditions.not(conditionToNegate)
+  return not condition(conditionToNegate)
+end
 
 function conditions.admin() return player.isAdmin() end
 function conditions.statPositive(stat) return status.statPositive(stat) end
 function conditions.statNegative(stat) return not status.statPositive(stat) end
 function conditions.species(species) return player.species() == species end
 function conditions.ownShip() return player.worldId() == player.ownShipWorldId() end
+function conditions.hasCompletedQuest(questId) return player.hasCompletedQuest(questId) end
 
 ---------------
 -- internals --
